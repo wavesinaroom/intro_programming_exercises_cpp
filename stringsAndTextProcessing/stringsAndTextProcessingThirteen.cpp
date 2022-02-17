c83ec3bcbff3afe587b553c686702f4ca9486051
Juan Jáuregui <juan@wavesinaroom.com>
2022-02-16 19:54:48 -0500

    [Feature] Add strings and text processing exercise thirteen

--- stringsAndTextProcessing/stringsAndTextProcessingThirteen.cpp
+++ stringsAndTextProcessing/stringsAndTextProcessingThirteen.cpp
@@ -10,9 +10,20 @@ For example, when http://www.cnn.com/video is passed, the result is:
 
 #include <iostream>
 
-void parse_url(std::string input_url)
+void cut_url(std::string input_url)
 {
-    //Find : (SLD or Second Level Domain)
-    //Find second . (TLD or Top Level Domain) 
-    //Find what comes next to the server
+    std::string protocol = input_url.substr(0, input_url.find(":"));
+    std::string server = input_url.substr(input_url.find("w"),input_url.find("com")-4);
+    std::string resource = input_url.substr(input_url.find("com/")+3, input_url.length()-1); 
+
+    std::cout<<"[Protocol] = "<<protocol<<'\n';
+    std::cout<<"[Server] = "<<server<<'\n';
+    std::cout<<"[Resource] = "<<resource<<'\n';
+
+}
+
+int main(int argc, char const *argv[])
+{
+    cut_url("http://www.theodinproject.com/video");
+    return 0;
 }
