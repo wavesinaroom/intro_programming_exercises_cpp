daeb5a8c3b297bee9c79b570dbd61e413dab57bc
Juan Jáuregui <juan@wavesinaroom.com>
2022-06-07 21:12:31 -0500

    [Fix] Change printNokiaSpecs return to void

--- definingClasses/8_19/GSM.cpp
+++ definingClasses/8_19/GSM.cpp
@@ -18,14 +18,7 @@
     GSM(const char * model...); 
     ~GSM();
   
-    static void printNokiaSpecs()
-    {
-      std::cout<<"Nokia N95 specs"<<'\n';
-      std::cout<<"Model: "<<nokiaN95.model.value()<<'\n';
-      std::cout<<"Manufacturer: "<<nokiaN95.manufacturer.value()<<'\n';
-      std::cout<<"Price: "<<nokiaN95.price.value()<<'\n';
-      std::cout<<"Owner: "<<nokiaN95.owner.value()<<'\n';
-    }
+    static void printNokiaSpecs();
 
   };
   
@@ -49,6 +42,15 @@
   
   }
   
+  void GSM::printNokiaSpecs()
+  {
+    std::cout<<"Nokia N95 specs"<<'\n';
+    std::cout<<"Model: "<<nokiaN95.model.value()<<'\n';
+    std::cout<<"Manufacturer: "<<nokiaN95.manufacturer.value()<<'\n';
+    std::cout<<"Price: "<<nokiaN95.price.value()<<'\n';
+    std::cout<<"Owner: "<<nokiaN95.owner.value()<<'\n';
+  }
+
   GSM GSM::nokiaN95 = GSM("N95", "Nokia", "USD$333", "Luke Skywalker", "Li-lon 950", "280 hours", "5 hours","40x53 mm", "3 buttons"); 
 
   int main(int argc, char const *argv[])
