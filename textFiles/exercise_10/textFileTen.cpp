4d9b7bd584b9da0a4778b076d7eaa3e93f3ad780
Waves in a room <jaureguij@javeriana.edu.co>
2022-10-05 21:12:23 -0500

    [Feature] Add text files exercise ten solution

--- textFiles/exercise_10/textFileTen.cpp
+++ textFiles/exercise_10/textFileTen.cpp
@@ -1,7 +1,7 @@
 /* 10. Write a program that extracts from an XML file the text only ("without the tags). Sample
    input file:
 
-   <?xml version="1.0"><student><name>Peter</name>
+<?xml version="1.0"><student><name>Peter</name>
    <age>21</age><interests count="3"><interest> Games</interest><interest>C#</interest><interest>
    Java</interest></interests></student>
 
@@ -18,39 +18,47 @@
 */
 
 #include <cstddef>
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
 
-int main(int argc, char* argv[]){
-
-  //look for the > symbol that precedes a letter or a number. Store > position in a variable
-  //track the word till reaching < character. That will be the string length
-  //cut the string with string.sbtr as it takes position and length as parameters
+void get_words(std::string & input, std::vector<std::string> & words){
   
-  std::string input="<?xml version=\"1.0\"><student><name>Peter</name>";
-  const char target_tag = '>';
+  const char closing_tag = '>';
   const char open_tag = '<';
   size_t char_count = 0;
-  size_t tag_pos = 0; 
-  std::vector<std::string> words; 
-  
+  size_t open_tag_pos = 0; 
+  size_t closing_tag_pos = 0;
 
- while (tag_pos!=input.length()-1) {
-  tag_pos = input.find(target_tag, tag_pos);
-  if ((input.at(tag_pos)+1)!=open_tag) {
-  char_count = 0;  
-   while (input.at(tag_pos+char_count+1)!=open_tag) {
-    ++char_count; 
-   } 
-  words.push_back(input.substr((tag_pos+1), char_count));
+  do{
+    open_tag_pos = input.find(open_tag,closing_tag_pos);
+    char_count = open_tag_pos-closing_tag_pos; 
+    if (char_count>1) {
+      words.push_back(input.substr(closing_tag_pos+1,char_count-1));
+      char_count = 0;
+    }
+    ++closing_tag_pos;
+    open_tag_pos = 0;
+    closing_tag_pos = input.find(closing_tag, closing_tag_pos);
+  } 
+ while (closing_tag_pos!=input.length()-1); 
+
+}
+
+int main(int argc, char* argv[]){
+  std::fstream read;
+  read.open("input");
+  std::string line;
+  std::vector<std::string> words;
+  if (read.is_open()) {
+    while (std::getline(read, line)) {
+      get_words(line,words);
+    }
+  } 
+  read.close();
+  for (std::string word : words) {
+    std::cout<<word<<'\n';
   }
-  std::cout<<tag_pos<<'\n';
-  ++tag_pos;
- } 
-
- for (std::string word : words) {
-  std::cout<<word<<'\n';
- }
   return 0; 
 }
