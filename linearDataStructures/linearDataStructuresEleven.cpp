6745f3bf549acb8914d39bc2e653c4dbc052428d
Waves in a room <jaureguij@javeriana.edu.co>
2023-01-31 21:53:13 -0500

    [Task] Draft remove method

--- linearDataStructures/linearDataStructuresEleven.cpp
+++ linearDataStructures/linearDataStructuresEleven.cpp
@@ -1,7 +1,14 @@
 /* Implement the data structure dynamic doubly linked list (DoublyLinkedList<T>) - list,
  * the elements of which have pointers both to the next and the previous elements. Implement 
  * the operations for adding, removing and searching for an element, as well as inserting
- * an element at a given index, retrieving an element by a given index and a method, which returns an array with the elements of the list*/
+ * an element at a given index, retrieving an element by a given index and a method, which returns an array with the elements of the list
+ * add -> done
+ * search -> done
+ * nodes array -> done
+ * remove ....
+ * insert at index : pending
+ * retrieve by index : pending
+ * */
 
 #include <cstddef>
 #include <iostream>
@@ -14,17 +21,27 @@ struct TNode{
   TNode *next = nullptr;
 };
   public:
+    std::vector<TNode *> elements;
     List();
     ~List();
-    std::vector<TNode *> elements;
     auto getRoot();
-    void getNodesVector(TNode *node);
-    auto searchNode(TValue data, TNode *node);    
     void addNode(TValue data);
+    void removeNode(TValue data);
+    auto searchNode(TValue data, TNode *node);    
+    void getNodesVector(TNode *node);
   private:
     auto getLastNode(TNode *node);
     TNode *root = nullptr;
 };
+
+template <typename TValue>
+auto List<TValue>::getLastNode(TNode *node){
+  if (node->next == nullptr) {
+    return node;
+  }else {
+    return getLastNode(node->next);
+  } 
+}
 
 template<typename TValue>
 List<TValue>::List(){
@@ -32,6 +49,11 @@ List<TValue>::List(){
 
 template<typename TValue>
 List<TValue>::~List<TValue>(){
+}
+
+template <typename TValue>
+auto List<TValue>::getRoot(){
+  return root;
 }
 
 template<typename TValue>
@@ -48,13 +70,19 @@ void List<TValue>::addNode(TValue data){
   }
 }
 
-template <typename TValue>
-auto List<TValue>::getLastNode(TNode *node){
-  if (node->next == nullptr) {
+template<typename TValue>
+void List<TValue>::removeNode(TValue data){
+  auto node = searchNode(data, getRoot());
+  node->previous->next = node->next;
+  node->next->previous = node->previous;
+  node = nullptr;
+}
+
+template<typename TValue>
+auto List<TValue>::searchNode(TValue data, TNode *node ){
+  if (node->data == data) 
     return node;
-  }else {
-    return getLastNode(node->next);
-  } 
+  return searchNode(data, node->next);
 }
 
 template <typename TValue>
@@ -66,23 +94,15 @@ void List<TValue>::getNodesVector(TNode *node){
   getNodesVector(node->next);
 }
 
-template <typename TValue>
-auto List<TValue>::getRoot(){
-  return root;
-}
 
-template<typename TValue>
-auto List<TValue>::searchNode(TValue data, TNode *node){
-  if (node->data == data) 
-    return node;
-  return searchNode(data, node->next);
-}
 
 int main(){
   List<int> list; 
   list.addNode(4);
   list.addNode(5);
   list.addNode(6);
+  list.addNode(7);
+  list.removeNode(5);
   auto found = list.searchNode(5, list.getRoot());
   list.getNodesVector(list.getRoot());
   return 0;
