32749f4425cf2a57661415028496775c936e6a34
Waves in a room <jaureguij@javeriana.edu.co>
2023-02-03 21:30:17 -0500

    [Fix] Change while condition in getNodesVector implementation to actually get last node of the list

--- linearDataStructures/linearDataStructuresEleven.cpp
+++ linearDataStructures/linearDataStructuresEleven.cpp
@@ -2,11 +2,11 @@
  * the elements of which have pointers both to the next and the previous elements. Implement 
  * the operations for adding, removing and searching for an element, as well as inserting
  * an element at a given index, retrieving an element by a given index and a method, which returns an array with the elements of the list
- * add : pending
- * nodes array : pending
+ * add : done
+ * nodes array : done
  * remove : pending 
  * insert at index : pending
- * retrieve by index : pending
+ * retrieve by index : ready
  * */
 
 #include <cstddef>
@@ -25,15 +25,23 @@ struct TNode{
     std::vector<TNode> elements;
     List();
     ~List();
-    void getNodeByIndex(int index);
+    auto getNodeByIndex(int index);
     void addNode(TValue data);
     void removeNode(TValue data);
     auto searchNode(TValue data);    
-    void getNodesVector(TNode *node);
+    void getNodesVector();
   private:
     void getLastNode();
     TNode *root = nullptr;
 };
+
+template <typename TValue>
+List<TValue>::List(){
+}
+
+template <typename TValue>
+List<TValue>::~List(){
+}
 
 template <typename TValue>
 void List<TValue>::getLastNode(){
@@ -44,11 +52,12 @@ void List<TValue>::getLastNode(){
 }
 
 template<typename TValue>
-void List<TValue>::getNodeByIndex(int index){
+auto List<TValue>::getNodeByIndex(int index){
   it = root;
   for (std::size_t i = 0; i<index; ++i) {
     it = it->next; 
   }
+  return *it;
 }
 
 template<typename TValue>
@@ -78,16 +87,20 @@ void List<TValue>::addNode(TValue data){
 template<typename TValue>
 void List<TValue>::removeNode(TValue data){
   searchNode(data);
-  it->previous->next = it->next;
-  it->next->previous = it->previous;
-  it = nullptr;
+  while (it->next) {
+    it->previous->next = it->next;
+    it->next->previous = it->previous;
+    it = it->next;
+  }
 }
 
 template <typename TValue>
-void List<TValue>::getNodesVector(TNode *node){
+void List<TValue>::getNodesVector(){
   it = root;
-  while(it->next)
+  while(it){
     elements.push_back(*it);
+    it = it->next;
+  }
 }
 
 int main(){
@@ -96,7 +109,8 @@ int main(){
   list.addNode(5);
   list.addNode(6);
   list.addNode(7);
-  list.removeNode(5);
+  list.getNodesVector();
+  
   return 0;
 }
 
