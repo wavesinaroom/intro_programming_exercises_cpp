991b9c41399027c64102a37ca5cfbc6913fcbb92
Waves in a room <jaureguij@javeriana.edu.co>
2023-02-07 21:29:47 -0500

    [Feature] Peek() method

--- linearDataStructures/linearDataStructuresTwelve.cpp
+++ linearDataStructures/linearDataStructuresTwelve.cpp
@@ -1,7 +1,7 @@
 /* Create a Dynamic<T> class to implement dynamically a stack (like a linked list, where each element knows its previous element and the stack knows its last element).
  * Add methods for all commonly used operations like Push(), Pop(), Peek(), Clear() and Count.
- * Push(T) - adds a new element on the top of the stack
- * Pop() - returns the highest element and removes it from the stack
+ * Push(T) - adds a new element on the top of the stack -> done
+ * Pop() - returns the highest element and removes it from the stack -> done
  * Peek() - returns the highest elements without removing it
  * Count - returns the count of elements in the stack
  * Clear() - returns all elements of the stack
@@ -18,13 +18,13 @@ struct TNode{
   public:
     Dynamic();
     ~Dynamic();
-    TNode *it = nullptr;
     int Count;
     void Push(TValue data);
     void Pop();
-    void Peek();
+    auto Peek();
     void Clear();
   private:
+    TNode *it = nullptr;
     TNode *top = nullptr;
 };
 
@@ -53,11 +53,17 @@ void Dynamic<TValue>::Pop(){
   it = nullptr;
 }
 
+template<typename TValue>
+auto Dynamic<TValue>::Peek(){
+  return top->data;
+}
+
 int main(){
   Dynamic<int> stack;
   stack.Push(8);
   stack.Push(9);
   stack.Pop();
+  int retrieved = stack.Peek();
 
   return 0;
 }
