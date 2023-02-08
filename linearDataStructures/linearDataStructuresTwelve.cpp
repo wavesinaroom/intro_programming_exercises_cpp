5d5d9b385b7ddbc8ece335beaa86da7bc2ed7309
Waves in a room <jaureguij@javeriana.edu.co>
2023-02-07 21:51:27 -0500

    [Feature] Clear method

--- linearDataStructures/linearDataStructuresTwelve.cpp
+++ linearDataStructures/linearDataStructuresTwelve.cpp
@@ -3,11 +3,12 @@
  * Push(T) - adds a new element on the top of the stack -> done
  * Pop() - returns the highest element and removes it from the stack -> done
  * Peek() - returns the highest elements without removing it -> done
- * Count - returns the count of elements in the stack
+ * Count - returns the count of elements in the stack-> done
  * Clear() - returns all elements of the stack
  */
 
 #include <iostream>
+#include <vector>
 
 
 template<typename TValue> class Dynamic{
@@ -22,7 +23,7 @@ struct TNode{
     void Push(TValue data);
     void Pop();
     auto Peek();
-    void Clear();
+    std::vector<TValue> Clear();
   private:
     TNode *it = nullptr;
     TNode *top = nullptr;
@@ -60,6 +61,17 @@ auto Dynamic<TValue>::Peek(){
   return top->data;
 }
 
+template<typename TValue>
+std::vector<TValue> Dynamic<TValue>::Clear(){
+  std::vector<TValue> elements;
+  it = top;
+  while (it) {
+    elements.push_back(it->data);
+    it = it->previous;
+  }
+  return elements;
+}
+
 int main(){
   Dynamic<int> stack;
   stack.Push(8);
@@ -68,6 +80,7 @@ int main(){
   stack.Push(10);
   stack.Push(11);
   int retrieved = stack.Peek();
+  std::vector<int> elements = stack.Clear();
 
   return 0;
 }
