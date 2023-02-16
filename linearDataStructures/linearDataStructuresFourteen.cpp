b22bdd5a3c765ea69a4fe99d95845df96b788aa5
Waves in a room <jaureguij@javeriana.edu.co>
2023-02-15 21:48:53 -0500

    [Task] Add exercise description

---
+++ linearDataStructures/linearDataStructuresFourteen.cpp
@@ -0,0 +1,6 @@
+/* Implement the structure "Circular Queue" with array, which doubles its capacity when it's 
+ * full. Implement the necessary methods for adding, removing the element in succesion
+ * and retrieving without removing the element in succession. If an operation is invalid,
+ * throw an appropriate exception.*/
+
+#include <iostream>
