2770e33487276aa5d6f17155975fe47814a654b8
wavesinaroom <jaureguij@javeriana.edu.co>
2023-03-02 21:52:12 -0500

    [Fix] Bring resize method inside add

--- linearDataStructures/linearDataStructuresFourteen.cpp
+++ linearDataStructures/linearDataStructuresFourteen.cpp
@@ -16,20 +16,8 @@ class CQueue {
     void add(TValue data);
   private:
     int size = 1;
-    //Definitely use a pointers array
-    TValue* array;
-    void resize(){
-      TValue *resize = new TValue[size*2];
-
-      for (int i = 0; i < size; ++i) {
-        resize[i] = array[i];
-      }
-      delete[] array;
-      array = resize;
-      size *=2;
-    }
+    TValue* array = new TValue[size];
 };
-
 template<typename TValue>
 CQueue<TValue>::CQueue(){
   array = new TValue [size];}
@@ -40,13 +28,22 @@ CQueue<TValue>::~CQueue(){}
 template<typename TValue>
 void CQueue<TValue>::add(TValue data){
   *array = data;
-  if(!++array)
-    resize();
-  ++array;
+  if (*array == array[size-1]) {
+    TValue *resize = new TValue[size*2];
+
+    for (int i = 0; i < size; ++i) {
+      resize[i] = array[i];
+    }
+    delete [] array;
+    array = resize;
+    size *= 2;
+  }
 }
 
 int main(){
   CQueue<std::string>test; 
   test.add("data");
+  test.add("Mafalda");
+  test.add("lol");
   return 0;
 }
