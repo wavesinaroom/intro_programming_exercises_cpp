3d0f606e32c3ce2cb71a2d19b8458f5428c5ae5d
wavesinaroom <jaureguij@javeriana.edu.co>
2023-03-16 21:51:19 -0500

    [Feature] Retrieve last value without removing it

--- linearDataStructures/linearDataStructuresFourteen.cpp
+++ linearDataStructures/linearDataStructuresFourteen.cpp
@@ -3,10 +3,10 @@
  * and retrieving without removing the element in succession. If an operation is invalid,
  * throw an appropriate exception.*/
 
+#include <cstdio>
 #include <cstdlib>
 #include <cstddef>
 #include <iostream>
-#include <string>
 #include <array>
 
 template <typename TValue>
@@ -18,13 +18,14 @@ class CQueue {
   public:
     CQueue();
     ~CQueue(); 
-    void expand();
     void add(TValue data);
+    auto retrieve();
   private:
     int size = 1;
     int index = 0;
     element* array = (element*) malloc(sizeof(element));
     element* it = array;
+    void expand();
 };
 template<typename TValue>
 CQueue<TValue>::CQueue(){
@@ -42,26 +43,36 @@ void CQueue<TValue>::expand(){
     array = extend; 
   }else {
     free(array);
-    puts("Error reallocation memory");
+    puts("Error reallocating memory");
   }
 }
 
 template<typename TValue>
 void CQueue<TValue>::add(TValue data){
+  if (it->index==index) 
+    ++it; 
   it->data = data;
   it->index = index;
-  if (it->index == size-1)
+  if (index == size-1)
     expand();
   ++it;
   ++index;
 }
 
+template<typename TValue>
+auto CQueue<TValue>::retrieve(){
+  if (it->index!=index) 
+    --it;
+  return *it;
+}
 
 int main(){
   CQueue<int>test; 
   test.add(2);
   test.add(4);
   test.add(6);
-  test.add(7);
+  test.add(8);
+  auto retrieved = test.retrieve(); 
+
   return 0;
 }
