5997c496b3fa682d674281c39dfebad9f51d659c
wavesinaroom <jaureguij@javeriana.edu.co>
2023-04-22 20:32:14 -0500

    [Feature] new findMind method

--- linearDataStructures/linearDataStructuresFifteen.cpp
+++ linearDataStructures/linearDataStructuresFifteen.cpp
@@ -1,5 +1,6 @@
 /* Implement sorting in a dynamic linked list wheadhout using an addheadional array or data structure.*/
 
+#include <algorithm>
 #include <iostream>
 
 class List{
@@ -11,12 +12,15 @@ class List{
     List(int data);
     ~List();
     void add(int data);
+    void sort();
+
   private:
-    
     Node* head;
-    Node* it;
+    Node* pre;
     Node* root = new Node;
     int min;
+    void findMin(Node * start);
+    void setPointers(Node * start);
 };
 
 List::List(int data){
@@ -35,6 +39,29 @@ void List::add(int data){
   head->next->data = data;
 }
 
+void List::findMin(Node * start){
+  head = start;
+  min = start->data;
+  while(head->next){
+    head = head->next;
+    if (head->data<min) 
+      min = head->data; 
+  }
+  setPointers(start);
+}
+
+void List::setPointers(Node * start){
+  head = start;
+  while (head->data!=min) {
+    pre = head;
+    head = head->next;
+  }
+}
+
+void List::sort(){
+  findMin(root);
+}
+
 int main(){
   List list(8);
   list.add(3);
@@ -42,5 +69,7 @@ int main(){
   list.add(7);
   list.add(2);
   list.add(4);
+  list.sort();
+
   return 0;
 }
