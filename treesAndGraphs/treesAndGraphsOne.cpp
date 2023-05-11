3c93f11b4707a6be0bebe38f4fb41ba5868cabe0
wavesinaroom <jaureguij@javeriana.edu.co>
2023-05-10 21:18:34 -0500

    [Task] Populate tree to write traversal function

--- treesAndGraphs/treesAndGraphsOne.cpp
+++ treesAndGraphs/treesAndGraphsOne.cpp
@@ -6,8 +6,8 @@
 
 class Node{
   int value;
-  std::vector<Node*> children;
   public:
+    std::vector<Node*> children;
     Node(int input){
       value = input; 
     }
@@ -31,9 +31,24 @@ class Tree{
     Tree(int value){
       root = new Node(value);
     }
+
+  
 };
 
 int main(){
   Tree tree(9); 
+  tree.root->add(8);
+  tree.root->add(7);
+  tree.root->add(6);
+  tree.root->add(7);
+  tree.root->children[0]->add(9);
+  tree.root->children[0]->add(7);
+  tree.root->children[0]->add(8);
+  tree.root->children[0]->add(7);
+  tree.root->children[0]->children[0]->add(5);
+  tree.root->children[0]->children[0]->add(7);
+  tree.root->children[0]->children[0]->add(4);
+  tree.root->children[0]->children[0]->add(4);
+  tree.root->children[0]->children[0]->add(7);
   return 0;
 }
