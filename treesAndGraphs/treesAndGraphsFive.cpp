c4c2e1124c73cfa567b57390f459ccd738c203a5
wavesinaroom <jaureguij@javeriana.edu.co>
2023-05-24 21:40:44 -0500

    [Task] Create a tree

---
+++ treesAndGraphs/treesAndGraphsFive.cpp
@@ -0,0 +1,25 @@
+/* Write a program that finds and prints all vertices of a binary tree which have 
+ * successor leaves only */
+
+#include <iostream>
+
+class Node{
+  public:
+    int data;
+    Node* left = nullptr;
+    Node* right = nullptr;
+
+    Node(int value){data=value;}
+};
+
+int main(){
+  Node root(1);
+  root.left = new Node(2);
+  root.right = new Node(3);
+  root.left->left = new Node(4);
+  root.left->right = new Node (5);
+  root.right->left = new Node(6);
+  root.right->right = new Node(7);
+
+  return 0;
+}
