19f74794bba057c5196c02db2e93e6a64b00cbee
wavesinaroom <jaureguij@javeriana.edu.co>
2023-05-26 21:42:37 -0500

    [Task] Commit after posting on Stackoverflow

--- treesAndGraphs/treesAndGraphsFive.cpp
+++ treesAndGraphs/treesAndGraphsFive.cpp
@@ -8,19 +8,35 @@ class Node{
     int data;
     Node* left = nullptr;
     Node* right = nullptr;
-    bool isLeave = false;
 
     Node(int value){data=value;}
 };
 
-int main(){
-  Node root(1);
-  root.left = new Node(2);
-  root.right = new Node(3);
-  root.left->left = new Node(4);
-  root.left->right = new Node (5);
-  root.right->left = new Node(6);
-  root.right->right = new Node(7);
+bool traverse(Node* node);
 
+bool traverse(Node* node){
+  if(!node->left&&!node->right)
+    return true; 
+
+  if (traverse(node->left)||traverse(node->right)) 
+      std::cout<<node->data<<'\t';  
+
+  if(node->left)
+    traverse(node->left);
+
+  if (node->right) 
+    traverse(node->right);  
+}
+
+int main(){
+  Node* root = new Node(1);
+  root->left = new Node(2);
+  root->right = new Node(3);
+  root->left->left = new Node(4);
+  root->left->right = new Node (5);
+  root->right->left = new Node(6);
+  root->right->right = new Node(7);
+
+  traverse(root);
   return 0;
 }
