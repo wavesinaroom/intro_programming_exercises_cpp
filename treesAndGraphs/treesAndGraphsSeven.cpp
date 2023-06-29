787c11db82b0b36e8e279b32524e473a17f1ffcd
wavesinaroom <jaureguij@javeriana.edu.co>
2023-06-28 21:37:23 -0500

    [Task] Draft dfs function, it only traverses the graph

--- treesAndGraphs/treesAndGraphsSeven.cpp
+++ treesAndGraphs/treesAndGraphsSeven.cpp
@@ -1,5 +1,5 @@
 /* Let's have as given a graph G(V,E) and two of its vertices x and y. Write a program that finds
- * the shortest path between two vertices measured in number of vertices staying on the path.*/
+ * the shortest path between two vertices measured in number of edges staying on the path.*/
 
 #include <iostream>
 #include <vector>
@@ -16,9 +16,31 @@ class Vertex{
     int getData(){ return data; }
 };
 
+void dfs(Vertex* v){
+  if(!v)
+    return;
+  std::cout<<v->getData()<<'\n';
+  for (auto &child : v->edges) {
+    dfs(child);
+  }
+}
+
 int main(){
-  Vertex main(1);
-  Vertex child(2);
-  main.add(&child);
+  Vertex root(1);
+  Vertex one(2);
+  Vertex two(3);
+  Vertex three(4);
+  Vertex subone(5);
+  Vertex subtwo(6);
+  Vertex subthree(7);
+  Vertex subsubone(8);
+  root.add(&one);
+  root.add(&two);
+  root.add(&three);
+  root.edges[0]->add(&subone);
+  root.edges[1]->add(&subtwo);
+  root.edges[2]->add(&subthree);
+  root.edges[0]->edges[0]->add(&subsubone);
+  dfs(&root);
   return 0;
 }
