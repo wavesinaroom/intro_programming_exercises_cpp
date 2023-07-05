2f4845b2be6898e0398ae09cf0de012a0b259aae
wavesinaroom <jaureguij@javeriana.edu.co>
2023-07-04 21:45:10 -0500

    [Task] Add parent to edges first position

--- treesAndGraphs/treesAndGraphsNine.cpp
+++ treesAndGraphs/treesAndGraphsNine.cpp
@@ -7,11 +7,38 @@ class Vertex {
   private:
     int data;
   public:
+    Vertex(int value){data = value;}
+    Vertex * parent;
     int getData(){return data;}
-    std::vector<Vertex *> children;
-    void add(Vertex * v){children.push_back(v);};
+    //position 0 always stores parent
+    std::vector<Vertex *> edges;
+    void add(Vertex * v){
+      edges.push_back(v);
+      edges.back()->add(this);
+    };
 };
 
 int main(){
 
+  Vertex root (0);
+  Vertex one (1);
+  Vertex two (2);
+  Vertex three (3);
+  Vertex four (4);
+  Vertex five (5);
+  Vertex six (6);
+  Vertex seven (7);
+  Vertex eight (8);
+
+  root.add(&one);
+  root.add(&two);
+  root.add(&three);
+  root.edges[0]->add(&four);
+  root.edges[0]->add(&five);
+  root.edges[0]->add(&six);
+  root.edges[0]->edges[0]->add(&seven);
+  root.edges[0]->edges[0]->add(&eight);
+  root.edges[0]->edges[0]->edges[0]->add(&root);
+
 }
+
