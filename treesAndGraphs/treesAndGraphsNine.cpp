e57c03cfd7fd45c041d72fdcab017061d60edf8d
wavesinaroom <jaureguij@javeriana.edu.co>
2023-07-09 21:47:39 -0500

    [Task] Bind vertex children to itself with parent pointer variable inside the class

--- treesAndGraphs/treesAndGraphsNine.cpp
+++ treesAndGraphs/treesAndGraphsNine.cpp
@@ -10,35 +10,17 @@ class Vertex {
     Vertex(int value){data = value;}
     Vertex * parent;
     int getData(){return data;}
-    //position 0 always stores parent
     std::vector<Vertex *> edges;
     void add(Vertex * v){
       edges.push_back(v);
-      edges.back()->add(this);
+      edges.back()->parent = this;
     };
 };
 
 int main(){
 
-  Vertex root (0);
+  Vertex root  (0);
   Vertex one (1);
-  Vertex two (2);
-  Vertex three (3);
-  Vertex four (4);
-  Vertex five (5);
-  Vertex six (6);
-  Vertex seven (7);
-  Vertex eight (8);
-
   root.add(&one);
-  root.add(&two);
-  root.add(&three);
-  root.edges[0]->add(&four);
-  root.edges[0]->add(&five);
-  root.edges[0]->add(&six);
-  root.edges[0]->edges[0]->add(&seven);
-  root.edges[0]->edges[0]->add(&eight);
-  root.edges[0]->edges[0]->edges[0]->add(&root);
-
 }
 
