46b45e178ea4e72735be0b3b29bfb19c397e1662
wavesinaroom <jaureguij@javeriana.edu.co>
2023-07-12 21:46:36 -0500

    [Task] Remove Node class

--- treesAndGraphs/treesAndGraphsNine.cpp
+++ treesAndGraphs/treesAndGraphsNine.cpp
@@ -3,20 +3,14 @@
 #include <iostream>
 #include <vector>
 
-template<typename T> class Node{
-  T* data;
-  public:
-    Node(T value){data=&value;}
-};
-
 template<typename T> class Edge{
-  Node<T> *from, *to;
+  T *from, *to;
 };
 
 template <typename T>
 class Graph{
 
-  std::vector<Node<T>*> nodes;
+  std::vector<T> nodes;
   std::vector<Edge<T>*> edges;
 };
 
