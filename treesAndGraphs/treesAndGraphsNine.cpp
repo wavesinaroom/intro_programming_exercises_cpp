09da6997b7930aca7cd74d933d78599dfb56d574
wavesinaroom <jaureguij@javeriana.edu.co>
2023-07-12 22:00:39 -0500

    [Task] Add Edge constructor, addNode addEdge graph class methods

--- treesAndGraphs/treesAndGraphsNine.cpp
+++ treesAndGraphs/treesAndGraphsNine.cpp
@@ -5,6 +5,10 @@
 
 template<typename T> class Edge{
   T *from, *to;
+  public:
+    Edge<T>(T &from, T &to){
+      this->from = from; this->to = to;
+    }
 };
 
 template <typename T>
@@ -12,5 +16,9 @@ class Graph{
 
   std::vector<T> nodes;
   std::vector<Edge<T>*> edges;
+
+  public:
+    void addNode(T data){nodes.push_back(data);}
+    void addEdge(T &from, T &to){edges.push_back(Edge<T>(from, to));}
 };
 
