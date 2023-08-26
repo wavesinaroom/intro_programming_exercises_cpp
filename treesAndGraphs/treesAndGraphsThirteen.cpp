f81e1261afa8e82289155c4e8d2543195dcce34c
wavesinaroom <jaureguij@javeriana.edu.co>
2023-08-25 21:37:07 -0500

    [Task] Connect nodes

--- treesAndGraphs/treesAndGraphsThirteen.cpp
+++ treesAndGraphs/treesAndGraphsThirteen.cpp
@@ -2,27 +2,59 @@
 
 #include <iostream>
 #include <vector>
+#include <iterator>
+#include <algorithm>
 
 class Node{
   int data;
   public:
     Node(int data){this->data = data;};
+    friend class Graph;
 };
 
 class Edge{
   Node* from;
   Node* to;
   public:
-  Edge(Node* from, Node* to){
-    this->from = from;
-    this->to = to;
-  }
+    Edge(){}
+    friend class Graph;
 };
+
 class Graph{
-  std::vector<Edge> edges;
-  std::vector<Node> nodes;
+  private:
+    bool isLoop;
+    std::vector<Edge> edges;
+    std::vector<Node> nodes;
+
   public:
     Graph(){};
     void addNode(int data){nodes.push_back(Node(data));};
-    void connectNodes(Node* from, Node* to){edges.push_back(Edge(from,to));};
+    void connectNodes(int from, int to){
+      edges.push_back(Edge());
+      for(std::vector<Node>::iterator it = nodes.begin(); it!=nodes.end(); ++it){
+        if(it->data == from)
+          edges.front().from = &*it;
+        else if (it->data == to)
+          edges.front().to = &*it;
+      }
+    };
 };
+
+int main(){
+
+  Graph graph;
+  graph.addNode(0);
+  graph.addNode(1);
+  graph.addNode(2);
+  graph.addNode(3);
+  graph.addNode(4);
+  graph.addNode(5);
+  graph.addNode(6);
+  graph.addNode(7);
+  graph.addNode(8);
+  graph.addNode(9);
+
+  graph.connectNodes(1, 2);
+
+  return 0;
+}
