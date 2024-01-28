bf2b20f402681706a52017504ed1e0f88e1ae6c1
wavesinaroom <jaureguij@javeriana.edu.co>
2024-01-28 17:38:30 -0500

    [Feature] Dictionaries exercise two solution

---
+++ dictionaries/dictionariesTwo.cpp
@@ -0,0 +1,41 @@
+/* Write a program to remove from a sequence all the integers, which appear odd number of times. For instance,for the sequence {4, 2, 2, 5, 2, 3, 2, 3, 1, 5, 2, 6, 6, 6} the output would be {5, 3, 3, 5}.*/
+
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+
+int main(){
+
+  std::unordered_map<int, int> map;
+  std::unordered_map<int, int>::iterator mit = map.begin();
+  std::vector<int> sequence = {4, 2, 2, 5, 2, 3, 2, 3, 1, 5, 2, 6, 6, 6};
+  std::vector<int>::iterator sit = sequence.begin();
+
+  for(; sit!=sequence.end(); ++sit){
+    mit = map.find(*sit);
+    if(mit!=map.end())
+      ++mit->second;
+    else
+      map.insert(std::make_pair(*sit,1));
+  }
+
+  mit = map.begin();
+  while(mit!=map.end()){
+    if(mit->second%2==1)
+      mit = map.erase(mit);
+    else
+      ++mit;
+  }
+
+  sit = sequence.begin();
+  while(sit!=sequence.end()){
+    mit = map.find(*sit);
+    if(mit==map.end())
+      sit = sequence.erase(sit);
+    else
+      ++sit;     
+  }
+
+  return 0;
+}
